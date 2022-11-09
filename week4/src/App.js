1d79b83e26e8eeeba3f9cf7681cc156429da2838
seobbang <seobbangway@naver.com>
2022-11-09 14:42:16 +0900

    CSS reset 추가

--- week4/src/App.js
+++ week4/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import UserInfo from "./pages/UserInfo";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/search" element={<MainPage />}></Route>
+        <Route path="/search" element={<MainPage />}>
+          <Route path=":userId" element={<UserInfo />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );

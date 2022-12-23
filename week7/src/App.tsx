01d58f02ed0924e8b5f99744744cb3e62463b163
seobbang <seobbangway@naver.com>
2022-12-24 02:10:17 +0900

    feat : 접속 페이지 UI 완성

---
+++ week7/src/App.tsx
@@ -0,0 +1,12 @@
+import Entry from "pages/Entry";
+import GlobalStyle from "./styles/globalStyle";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Entry />
+    </>
+  );
+};
+export default App;

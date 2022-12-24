cfe9b158305dbd6c4261c504c7e78750eca60087
seobbang <seobbangway@naver.com>
2022-12-25 02:02:32 +0900

    4차 세미나 과제로 변경

--- week7/src/App.tsx
+++ week7/src/App.tsx
@@ -10,3 +10,21 @@ const App = () => {
   );
 };
 export default App;
+// import "./App.css";
+// import { useState } from "react";
+// import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
+// function App() {
+//const [history, setHistory] = useState(["hi", "me"]);
+//const arrString = JSON.stringify(history);
+
+//window.localStorage.setItem("history", arrString);
+
+//console.log(arrString);
+
+//   return (
+//
+//   );
+// }
+
+// export default App;
--- week7/src/components/Router.tsx
+++ week7/src/components/Router.tsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Entry from "pages/Entry";
 import Write from "pages/Write";
+import Main from "pages/Main";
+import UserInfo from "components/UserInfo";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Entry />} />
-        <Route path="/write" element={<Write />} />
+        {/* <Route path="/" element={<Entry />} />
+        <Route path="/write" element={<Write />} /> */}
+        <Route path="/search" element={<Main />}>
+          <Route path=":userId" element={<UserInfo />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
